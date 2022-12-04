0642a62eb36e6146b20376012405bf4253267d4a
Andrew Stelter <andrew.d.stelter@gmail.com>
2022-12-04 12:01:34 -0600

    Add cmake code to build according to patterns

---
+++ day_1/day_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+void day_1(std::istream& input, std::ostream& output)
+{
+}
+
+void day_1_adv(std::istream& input, std::ostream& output)
+{
+}
+
+TEST(Day1, Dummy)
+{
+}
